bb557fbbfe6307b909e355de2db07d3fae8e47e5
henrywigg <henrique.wiggers.99@gmail.com>
2025-11-30 14:39:35 -0300

    Evento de cadastro de tarefa

--- src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Tarefa } from '../shared';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Tarefa, TarefaService } from '../shared';
 
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cadastrar-tarefa',
   templateUrl: './cadastrar-tarefa.component.html',
@@ -8,10 +10,22 @@ import { Tarefa } from '../shared';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
+ @ViewChild('formTarefa') formTarefa: NgForm;
   tarefa: Tarefa;
-  constructor() { }
+  constructor(
+    private tarefaService: TarefaService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.tarefa = new Tarefa();
+  }
+
+  cadastrar(): void{
+    if(this.formTarefa.form.valid){
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"]);
+    }
   }
 
 }
